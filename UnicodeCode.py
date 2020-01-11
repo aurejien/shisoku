cfe812a0daa18ece67ef67fef62d72569b10a3c7
Marshmallow <au8relien@gmail.com>
2020-01-11 17:56:55 +0100

    Unicode-code

---
+++ UnicodeCode.py
@@ -0,0 +1,16 @@
+# code design by aurejien
+# find the unicode code to your Character
+
+print()
+print()
+print("—————————————————————————————————————————————————————————————————————————————")
+print()
+word = input("Enter only on Character to convert to ASCII \n →    ")
+print()
+print("—————————————————————————————————————————————————————————————————————————————")
+print()
+print("Your unicode code: \n→    ", ord(word))
+print()
+print("—————————————————————————————————————————————————————————————————————————————")
+print()
+print()
