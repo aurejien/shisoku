438b1afc2a48749f773fd30c76887c2585b66e3f
aurejien <au8relien@gmail.com>
2020-01-22 08:31:22 +0100

    game

--- Games/loknightV1.py
+++ Games/loknightV1.py
@@ -15,16 +15,16 @@ print()
 print("----------------------------------------------------")
 player_name = input("Your name. \n> ").lower().title()
 xp = 0
-gems = 0
+gems = 10
 bypass = 1
 rules = input("{1} Pour afficher les règles\n{2} Pour passer\n>  ")
 if rules == "1":
   print()
   print()
   print(f"Bienvenue sur {game_name}, le jeu est encore en développement")
-  print("Les règles sont simple, tu choisis un héro, tu choisis entrainement pour te battre contre des bots")
+  print("Les règles sont simple, tu choisis un héro,\ntu choisis entrainement pour te battre contre des bots")
   print("Tu as 3 coups qui coutent des cristaux pour pouvoir attaquer")
-  print("C'est un tour par tour, tu attaques, le bot il t'attaque, celui qui a plus de vie a perdu, ou plus de cristaux")
+  print("C'est un tour par tour, tu attaques, le bot il t'attaque,\ncelui qui a plus de vie a perdu, ou plus de cristaux")
   print("Pour une attaque de base tu dépenses 1 cristal et tu en gagnes 3,\nPour une grosse attaque tu dépenses 5 cristaux mais tu en gagnes que 1,\nPour une défense tu dépenses 2 cristaux mais tu en gagnes 3.")
   print("Bon jeu..")
   print("C'est un peu le fouilli désolé")
@@ -45,8 +45,9 @@ print()
 print("----------------------------------------------------")
 print()
 print("Choisi ton hero")
-print("{1} Cavalier: Attaque+++    Vie++++")
-print("{-} Archer:   Bientôt disponible")
+print("{1} Cavalier: Attaque: +++    Vie: ++++")
+print("{2} Archer:   Attaque: ++++   Vie: +++")
+print()
 print("{-} Mage:     Bientôt disponible")
 print("{-} Barbare:  Bientôt disponible")
 hero_choix = -1
@@ -56,33 +57,33 @@ while hero_choix <= 0:
         hero_choix = int(hero_choix)
         if hero_choix == 1:
 
-            hero = "Cavalier"  # Cavalier
+            hero = "Le Cavalier"       # Cavalier
             attaque_un = 25            # coup de point
             attaque_deux = 50          # epee
             contre = 60                # bouclier
 
             arme_un = "de point"
-            arme_deux = "épée"
+            arme_deux = "d'épée"
             arme_trois = "bouclier"
 
             arme_Un_un = "le coup de point"
             arme_Un_deux = "l'épée"
             arme_Un_trois = "le bouclier"
 
-            #elif hero_choix == 2:
+        elif hero_choix == 2:
 
-            #hero = "Archer"  # Archer
-            #attaque_un = 30            # fleche normal
-            #attaque_deux = 75          # fleche de feux
-            #contre = 90                # Pas chassé
+            hero = "L'Archer"  # Archer
+            attaque_un = 30            # fleche normal
+            attaque_deux = 65          # fleche de feux
+            contre = 90                # Pas chassé
 
-            #arme_un = "flèche"
-            #arme_deux = "fléche de feu"
-            #arme_trois = "invisible"
+            arme_un = "Simple flèche"
+            arme_deux = "La fléche de feu"
+            arme_trois = "Le pas chassé"
 
-            #arme_Un_un = "la flèche"
-            #arme_Un_deux = "la flèche de feu"
-            #arme_Un_trois = "se rend invisible"
+            arme_Un_un = "Simple flèche"
+            arme_Un_deux = "la flèche de feu"
+            arme_Un_trois = "Le pas chassé"
 
         else:
             raise ValueError
@@ -100,34 +101,34 @@ while True:
     if hero_choix == 1:
         vie = 200
     elif hero_choix == 2:
-        vie = 120
+        vie = 150
 
     print("----------------------------------------------------")
-    print(f"- {player_name}")
-    print()
-    print(f"- {xp}xp")
-    print(f"- {gems}◊ Les cristaux sont utilisés pour attaquer")
-    print(f"- Niveau { level}")
-    print(f"- Argent {argent}$")
+    print(f"{player_name}")
+    print(f"  {gems}◊")
+    print(f"  {xp}xp")
+    print(f"  Argent {argent}$")
     print()
     time.sleep(2)
-    print(f"{hero}:")
-    print(f"    vie: {vie}")
-    print(f"-  1◊  {arme_Un_un}: -{attaque_un}")
-    print(f"-  5◊  {arme_Un_deux}: -{attaque_deux}")
-    print(f"-  3◊  {arme_Un_trois} reduit de {contre}% l'attaque ennemi")
+    print(f"{hero}: vie {vie}")
+    print(f"  ↓1◊ ↑3◊  {arme_Un_un}: -{attaque_un}")
+    print(f"  ↓5◊ ↑1◊  {arme_Un_deux}: -{attaque_deux}")
+    print(f"  ↓2◊ ↑3◊  {arme_Un_trois} reduit de {contre}% l'attaque ennemi")
     print()
     time.sleep(2)
-    print("{-} Combat. Bientôt disponible\n{2} Entrainement\n")
+    print("Combats")
+    print("  {-} Combat. Bientôt disponible\n  {2} Entrainement\n")
     print()
+    time.sleep(0.5)
     print("Shop Bientôt disponible")
-    print("{-} Elixir de vie: 100$ \n{-} Épée fer:+25dp 300$\n{-} Épée or: +50dp 600$   ")
-    print("----------------------------------------------------")
+    print("  {-} Elixir de vie: 100$ \n  {-} Épée fer:+25dp 300$\n  {-} Épée or: +50dp 600$   ")
     print()
-    time.sleep(2)
     menu_choix = -1
     while menu_choix <= 0:
-        menu_choix = input("> ")
+        menu_choix = input("Faite votre choix\n> ")
+        print("----------------------------------------------------")
+        print()
+        time.sleep(2)
         try:
             menu_choix = int(menu_choix)
             if menu_choix == 1:
@@ -146,13 +147,9 @@ while True:
                         bot_level = int(bot_level)
                         # =========================== BOT FACILE
                         if bot_level == 1:
-                            gems = 15
                             print()
                             print("Création du bot. . .")
                             time.sleep(1.5)
-                            print("-------------")
-                            print(f"{gems}◊")
-                            print("-------------")
                             print()
 
                             list_attaque = [10, 25, 50, 20, 35]
@@ -160,46 +157,38 @@ while True:
                             bot_vie = vie - 100
 
 
-                            xp = 1000
-                            argent = 200
+                            xp_up = 200
+                            argent_up = 75
 
                             break
                         # =========================== BOT NORMAL
                         elif bot_level == 2:
-                            gems = 10
                             print()
                             print("Création du bot. . .")
                             time.sleep(1.5)
-                            print("-------------")
-                            print(f"{gems}◊")
-                            print("-------------")
                             print()
 
                             list_attaque = [50, 15, 20, 60, 40]
                             bot_hero = "bot_444"
                             bot_vie = vie - 60
 
-                            xp = 5000
-                            argent = 500
+                            xp_up = 500
+                            argent_up = 150
 
                             break
                         # =========================== BOT DIFFICILE
                         elif bot_level == 3:
-                            gems = 10
                             print()
                             print("Création du bot. . .")
                             time.sleep(1.5)
-                            print("-------------")
-                            print(f"{gems}◊")
-                            print("-------------")
                             print()
 
                             list_attaque = [20, 60, 75, 50, 55, 65]
                             bot_hero = "bot_666"
                             bot_vie = vie - 40
                             
-                            xp = 10000
-                            argent = 1000
+                            xp_up = 1000
+                            argent_up = 300
 
                             break
                         # ===========================
@@ -212,16 +201,15 @@ while True:
                 print()
                 print(f"{hero} contre {bot_hero}")
                 print("Ready? {1}")
-                play = int(input("> "))
-                print("------------------")
-                print(f"{hero}: vie {vie}  ")
-                print(f"{bot_hero}: vie {bot_vie}  ")
-                print(f"{gems}◊ restant")
+                play = input("> ")
                 tour = 1
                 while tour != 0:
                     if vie <= 0:
+                        argent_up = 0
+                        xp_up = 0
                         print()
                         print(f"{player_name} votre {hero} est mort\n Tu me gagnes rien.")
+                        print(f"{player_name} vous avez gagné\nVous remportez {xp_up} {argent_up}$")
                         print()
                         print("Retour au menu. . .")
                         print()
@@ -229,17 +217,20 @@ while True:
                         break
                     elif bot_vie <= 0:
                         print()
-                        print(f"{player_name} vous avez gagné\nVous remportez 500xp 50$")
+                        print(f"{player_name} vous avez gagné\nVous remportez {xp_up} {argent_up}$")
+                        argent += argent_up
+                        xp += xp_up
                         print()
                         print("Retour au menu. . .")
                         print()
                         time.sleep(2)
-                        argent += 50
-                        xp += 500
                         break
                     elif bypass <= 0:
+                        argent_up = 0
+                        xp_up = 0
                         print()
                         print(f"{player_name} vous n'avez plus assé de cristaux pour attaquer")
+                        print(f"{player_name} vous avez gagné\nVous remportez {xp_up} {argent_up}$")
                         print()
                         print("Retour au menu. . .")
                         print()
@@ -247,13 +238,22 @@ while True:
                         break
                     combat = -1
                     while combat <= 0:
-                      
+                        print("----------")
+                        print(f"tour {tour}  |")
+                        print("----------------------------------------------------")
                         print()
-                        print("----------------------------")
-                        print(f"tour {tour}")
-                        print("----------------------------")
-                        combat = int(input(
-                            f"{{1}}: 1◊ {arme_Un_un}\n{{2}}: 5◊ {arme_Un_deux}\n{{3}}: 2◊ {arme_Un_trois}\n>"))
+                        print(f"   vie {vie} - {hero}")
+                        print(f"   vie {bot_vie} - {bot_hero}")
+                        print()
+                        print(f"   {gems}◊ restant")
+                        print()
+                        print("----------------------------------------------------")
+                        print(f"{{1}} |  ↓1◊ ↑3◊  {arme_Un_un}: -{attaque_un}")
+                        print(f"{{2}} |  ↓5◊ ↑1◊  {arme_Un_deux}: -{attaque_deux}")
+                        print(f"{{3}} |  ↓2◊ ↑3◊  {arme_Un_trois} reduit de {contre}% l'attaque ennemi")
+                        print()
+                        combat = int(input("> "))
+                        print("----------------------------------------------------")
                         try:
                             combat = int(combat)
                             if combat == 1:
@@ -268,25 +268,34 @@ while True:
                                 vie -= bot_attaque
                                 degats = attaque_un
                                 bot_degats = bot_attaque
-                                print(f"{hero} inflige un coup {arme_un}")
-                                print(f"Dégats -{degats}")
                                 print()
-                                print(f"{bot_hero} attaque. . .")
+                                print(f"{hero}")
+                                print(f"Vous attaquez. .. !\n")
+                                time.sleep(1)
+                                print("---- - ->")
+                                print("---- - ->")
+                                print()
+                                print(f"Un coup {arme_un}  // {degats} Dégats")
                                 print()
                                 time.sleep(2)
-                                print(f"{bot_hero} inflige un coup aléatoire")
-                                print(f"Dégats -{bot_degats}")
+                                print("----- tour ennemi -------")
                                 print()
-                                print("------------------")
-                                print(f"{hero}: vie {vie}  ")
-                                print(f"{bot_hero}: vie {bot_vie}  ")
-                                print(f"{gems}◊ restant")
-                                time.sleep(2.4)
+                                print(f"{bot_hero}")
+                                print(f"Il attaque. .. !\n")
+                                time.sleep(1)
+                                print("---- - ->")
+                                print("---- - ->")
+                                print()
+                                print(f"Un coup aléatoire // {bot_degats} Dégats")
+                                print()
+                                print(f"Fin du tour {tour}")
+                                print("----------------------------------------------------")
+                                print("chargement. . .")
+                                time.sleep(5)
                                 print()
                                 tour += 1
                                 gems += 3
                             elif combat == 2:
-                                
                                 if gems <= 4:
                                   print(f"{gems}◊ restant")
                                   print("Tu ne peux pas utiliser l'épée, tu as plus de cristal")
@@ -297,20 +306,30 @@ while True:
                                 vie -= bot_attaque
                                 degats = attaque_deux
                                 bot_degats = bot_attaque
-                                print(f"{hero} inflige un coup {arme_deux}")
-                                print(f"Dégats -{degats}")
                                 print()
-                                print(f"{bot_hero} attaque. . .")
+                                print(f"{hero}")
+                                print(f"Vous attaquez. .. !\n")
+                                time.sleep(1)
+                                print("---- - ->")
+                                print("---- - ->")
+                                print()
+                                print(f"Un coup {arme_deux}  // {degats} Dégats")
                                 print()
                                 time.sleep(2)
-                                print(f"{bot_hero} inflige un coup aléatoire")
-                                print(f"Dégats -{bot_degats}")
+                                print("----- tour ennemi -------")
                                 print()
-                                print("------------------")
-                                print(f"{hero}: vie {vie}  ")
-                                print(f"{bot_hero}: vie {bot_vie}  ")
-                                print(f"{gems}◊ restant")
-                                time.sleep(2.4)
+                                print(f"{bot_hero}")
+                                print(f"Il attaque. .. !\n")
+                                time.sleep(1)
+                                print("---- - ->")
+                                print("---- - ->")
+                                print()
+                                print(f"Un coup aléatoire // {bot_degats} Dégats")
+                                print()
+                                print(f"Fin du tour {tour}")
+                                print("----------------------------------------------------")
+                                print("chargement. . .")
+                                time.sleep(5)
                                 print()
                                 tour += 1
                                 gems += 1
@@ -321,26 +340,35 @@ while True:
                                   break
                                 gems -= 2
                                 bot_attaque = choice(list_attaque)
-                                bot_degats = 0
-                                vie = vie - int(bot_attaque -
-                                                ((bot_attaque * contre)/100))
-                                degats = int(
-                                    bot_attaque-((bot_attaque * contre)/100))
-                                print(f"{bot_hero} attaque. . .")
+                                degats = 0
+                                bot_degats = int(bot_attaque-((bot_attaque * contre)/100))
+                                bot_vie += degats
+                                vie -= bot_degats
+
+                                print()
+                                print(f"{hero}")
+                                print(f"Vous attaquez. .. !\n")
+                                time.sleep(1)
+                                print("---- - ->")
+                                print("---- - ->")
+                                print()
+                                print(f"Un coup {arme_trois}  // {degats} Dégats")
                                 print()
                                 time.sleep(2)
-                                print(f"{bot_hero} inflige un coup aléatoire")
-                                print(f"Dégats -{degats}")
+                                print("----- tour ennemi -------")
                                 print()
-                                print(f"{hero} utilise la défence {arme_trois}")
-                                print(f"Dégats -{bot_degats}")
+                                print(f"{bot_hero}")
+                                print(f"Il attaque. .. !\n")
+                                time.sleep(1)
+                                print("---- - ->")
+                                print("---- - ->")
                                 print()
-                                print("------------------")
-                                print(f"{hero}: vie {vie}  ")
-                                print(f"{bot_hero}: vie {bot_vie}  ")
-                                print(f"{gems}◊ restant")
-                                time.sleep(2.4)
+                                print(f"Un coup aléatoire //  {bot_attaque} / -{bot_degats}Dégats")
                                 print()
+                                print(f"Fin du tour {tour}")
+                                print("----------------------------------------------------")
+                                print("chargement. . .")
+                                time.sleep(5)
                                 tour += 1
                                 gems += 3
                             else:
